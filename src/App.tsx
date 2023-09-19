1909fecf4470113300ef70665865b2b40cc50894
itsaFan <steffansim@gmail.com>
2023-09-19 20:49:05 +0800

    intialize project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import LandingPage from './pages/landing-page'
+
+function App() {
+
+
+  return (
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+    </Routes>
+  )
+}
+
+export default App
