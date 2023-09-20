97b18fd7333842db28cb331bbac41edd7e6c9611
itsaFan <steffansim@gmail.com>
2023-09-20 19:29:09 +0800

    add authenticated routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/landing-page";
 import DashboardPage from "./pages/dashboard-page";
+import { useContext } from "react";
+import AuthContext from "./context/auth-context";
 
 function App() {
+  const { isLoggedIn, currentUser } = useContext(AuthContext);
+
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/dashboard" element={<DashboardPage />}/>
+
+      {/* Authenticated Routing */}
+      {isLoggedIn && (
+        <>
+          {/* Role aunthentication */}
+          {currentUser.role === "user" && (
+            <>
+              <Route path="/dashboard" element={<DashboardPage />} />
+            </>
+          )}
+        </>
+      )}
     </Routes>
   );
 }
