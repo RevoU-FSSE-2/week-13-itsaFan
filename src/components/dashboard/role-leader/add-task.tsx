1b34f2cb0d168e732da7ecce5b74fd6b8cf6ad26
itsaFan <steffansim@gmail.com>
2023-09-21 21:49:50 +0800

    create task

---
+++ src/components/dashboard/role-leader/add-task.tsx
@@ -0,0 +1,142 @@
+import { createTask, getProjects } from "../../../api/leader-api";
+import AuthContext from "../../../context/auth-context";
+import { useContext, useEffect, useState, ChangeEvent, FormEvent } from "react";
+import Modal from "../../UI/modal";
+import CardBorder from "../../UI/card-border";
+import { Button } from "antd";
+import SuccessModal from "../../UI/success-modal";
+import { Project } from "../../../helpers/form-interface";
+
+export default function AddTaskForm() {
+  const { token } = useContext(AuthContext);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [selectedProjectId, setSelectedProjectId] = useState<string>("");
+  const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    project: "",
+    title: "",
+    description: "",
+    dueDate: "",
+    priority: "",
+    assignedTo: "",
+  });
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await getProjects(token);
+        // console.log(response);
+        setProjects(response.projects);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    fetchProjects();
+  }, [token]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    if (name === "project") {
+      setSelectedProjectId(value);
+    }
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await createTask(token, formData);
+      console.log("Task created successfully");
+      setFormData({
+        project: "",
+        title: "",
+        description: "",
+        dueDate: "",
+        priority: "",
+        assignedTo: "",
+      });
+      setIsSuccessModalOpen(true);
+    } catch (error) {
+      console.error("Error creating task:", error);
+    }
+  };
+
+  const handleCloseSuccessModal = () => {
+    setIsSuccessModalOpen(false);
+  };
+
+  return (
+    <Modal className="">
+      <CardBorder title="Add Task">
+        <form onSubmit={handleFormSubmit}>
+          <div className="">
+            <label className="font-semibold">Project:</label>
+            <div>
+              <select name="project" value={formData.project} onChange={handleInputChange} className="w-full h-10 border border-gray-800 px-3 rounded-lg" required>
+                <option value="">Select a project</option>
+                {projects.map((project) => (
+                  <option key={project._id} value={project._id}>
+                    {project.projectName}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div>
+            <label className="font-semibold">Title:</label>
+            <div>
+              <input type="text" name="title" value={formData.title} onChange={handleInputChange} className="w-full h-10 border border-gray-800 px-3 rounded-lg" required />
+            </div>
+          </div>
+          <div>
+            <label className="font-semibold">Description:</label>
+            <div>
+              <input type="text" name="description" value={formData.description} onChange={handleInputChange} className="w-full h-10 border border-gray-800 px-3 rounded-lg" required />
+            </div>
+          </div>
+          <div>
+            <label className="font-semibold">Due Date:</label>
+            <div>
+              <input type="date" name="dueDate" value={formData.dueDate} onChange={handleInputChange} className="w-full h-10 border border-gray-800 px-3 rounded-lg" required />
+            </div>
+          </div>
+          <div>
+            <label className="font-semibold">Priority:</label>
+            <div>
+              <select name="priority" value={formData.priority} onChange={handleInputChange} className="w-full h-10 border border-gray-800 px-3 rounded-lg" required>
+                <option>low</option>
+                <option>medium</option>
+                <option>high</option>
+              </select>
+            </div>
+            {/* <input type="text" name="priority" value={formData.priority} onChange={handleInputChange} /> */}
+          </div>
+          <div>
+            <label className="font-semibold">Assigned To:</label>
+            <div>
+              <select name="assignedTo" value={formData.assignedTo} onChange={handleInputChange} className="w-full h-10 border border-gray-800 px-3 rounded-lg" required>
+                <option value="">Select a member</option>
+                {selectedProjectId &&
+                  projects
+                    .find((project) => project._id === selectedProjectId)
+                    ?.assignedGroup.members.map((member) => (
+                      <option key={member._id} value={member._id}>
+                        {member.username}
+                      </option>
+                    ))}
+              </select>
+            </div>
+          </div>
+          <div>
+            <Button type="primary" htmlType="submit" className="w-full h-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5">
+              Create Task
+            </Button>
+
+            <SuccessModal isOpen={isSuccessModalOpen} onClose={handleCloseSuccessModal} title="Success!" textBody="Your Task Has Been Created Successfully" />
+          </div>
+        </form>
+      </CardBorder>
+    </Modal>
+  );
+}
