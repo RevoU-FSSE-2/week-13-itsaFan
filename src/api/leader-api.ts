385b23ee99fa6088225f74583c2298a5b6c04f10
itsaFan <steffansim@gmail.com>
2023-09-21 21:49:34 +0800

    add new api

--- src/api/leader-api.ts
+++ src/api/leader-api.ts
@@ -12,3 +12,37 @@ export const getTasks = async (token: string) => {
     throw error;
   }
 };
+
+export const getProjects = async (token: string) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/leader/projects`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error when trying to get projects", error);
+    throw error;
+  }
+};
+
+type FormData = {
+  project: string,
+  title: string,
+  description: string,
+  dueDate: string,
+  priority: string,
+  assignedTo: string,
+}
+
+export const createTask = async (token: string, formData: FormData  ) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/leader/tasks/add`, formData, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    console.log("Task created successfully:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating task:", error);
+    throw error;
+  }
+};
